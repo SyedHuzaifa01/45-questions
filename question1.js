b7240d6ed015cde097b32a33932015a8b894a6b3
SyedHuzaifa01 <163592874+SyedHuzaifa01@users.noreply.github.com>
2024-03-16 02:46:47 +0500

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,3 @@
+// var mymessage = ("Hello Huzaifa,");
+// var mymessage1 = ("lets learn something new today");
+// console.log(mymessage + mymessage1);
---
+++ question1.ts
@@ -0,0 +1,4 @@
+// let mymessage = ("Hello Huzaifa,");
+// let mymessage1 = ("lets learn something new today");
+// console.log(mymessage + mymessage1);
+
